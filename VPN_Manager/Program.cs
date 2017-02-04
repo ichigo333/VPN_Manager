350326133720e0888a677969eb1ce9a1244f7f18
Alex <alex333sh@gmail.com>
2017-02-04 03:18:12 -0500

    experimenting with process kill
    noit working as expected

--- VPN_Manager/Program.cs
+++ VPN_Manager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Threading;
 
@@ -6,19 +7,51 @@ namespace VPN_Manager {
   class MainClass {
     public static void Main() {
       var vpn = new Vpn();
+      var vpnPath = "C:\\Program Files\\pia_manager\\pia_manager.exe";
 
-      for (int i = 0; i < 20; i++) {
-        Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
-        Console.WriteLine(vpn);
-        //Console.WriteLine("Current IP       : " + GetCurrentIP());
-
+      while (true) {
+        KillApplication("pia_manager.exe");
+        //KillApplication("pia_nw");
+        //KillApplication("openvpn");
         Thread.Sleep(10000);
+        StartVpn(vpn, vpnPath);
+
+        while (vpn.IsConnected()) {
+          Console.Write("VPN is connected");
+          for (int i=0; i < 20; i++) {
+            Console.Write(".");
+            Thread.Sleep(1000);
+          }
+          Console.WriteLine();
+        }
       }
-      Console.WriteLine("DONE");
-      Console.ReadKey();
+     
     }
 
+    public static void StartVpn(Vpn vpn, string vpnPath) {
+      StartApplication(vpnPath);
 
+      while (!vpn.IsConnected()) {
+        Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
+        Thread.Sleep(10000);
+      }
+
+      Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
+      Console.WriteLine(vpn);
+    }
+
+    public static void StartApplication(string path) {
+      Process.Start(path);
+    }
+
+    public static void KillApplication(string name) {
+      var processes = Process.GetProcessesByName(name);
+ 
+      foreach (var process in processes) {
+        process.
+        process.Kill();
+      }
+    }
 
     public static string GetCurrentIP() {
       var client = new WebClient();
