b76f8b633da5c719508d364532661b65e9cdcdb9
Alex <alex333sh@gmail.com>
2017-02-04 01:48:36 -0500

    breaking out some functionality into vpn class

--- VPN_Manager/Program.cs
+++ VPN_Manager/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Threading;
 
 namespace VPN_Manager {
   class MainClass {
     public static void Main() {
+      var vpn = new Vpn();
 
       for (int i = 0; i < 20; i++) {
-        Console.WriteLine("Is VPN Connected : " + IsVPNConnected());
-        Console.WriteLine("Current IP       : " + GetCurrentIP());
+        Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
+        Console.WriteLine(vpn);
+        //Console.WriteLine("Current IP       : " + GetCurrentIP());
 
         Thread.Sleep(10000);
       }
@@ -17,28 +18,7 @@ namespace VPN_Manager {
       Console.ReadKey();
     }
 
-    public static bool IsVPNConnected() {
-      if (NetworkInterface.GetIsNetworkAvailable()) {
-        
-        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
-        foreach (NetworkInterface Interface in interfaces) {
-          if (Interface.OperationalStatus == OperationalStatus.Up &&
-              Interface.Description == "TAP-Windows Adapter V9") {
-            
-            IPv4InterfaceStatistics statistics = Interface.GetIPv4Statistics();
-            Console.WriteLine("================================================");
-            Console.WriteLine("Name        : " + Interface.Name);
-            Console.WriteLine("Description : " + Interface.Description);
-            Console.WriteLine("ID          : " + Interface.Id);
-            Console.WriteLine("Status      : " + Interface.OperationalStatus);
-            Console.WriteLine("================================================");
-                             
-            return true;
-          }
-        }
-      }
-      return false;
-    }
+
 
     public static string GetCurrentIP() {
       var client = new WebClient();
---
+++ VPN_Manager/Vpn.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+namespace VPN_Manager {
+  public class Vpn {
+
+    public bool IsConnected() {
+      if (GetVpnInterface() != null) {
+        return true;
+      }
+      return false;
+    }
+
+    NetworkInterface GetVpnInterface() {
+      if (NetworkInterface.GetIsNetworkAvailable()) {
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface Interface in interfaces) {
+          if (Interface.OperationalStatus == OperationalStatus.Up && Interface.Description == "TAP-Windows Adapter V9") {
+            return Interface;
+          }
+        }
+      }
+      return null;
+    }
+
+    public override string ToString() {
+      var Interface = GetVpnInterface();
+      var stringBuilder = new StringBuilder();
+
+      if (Interface != null) {
+        stringBuilder.AppendLine("================================================");
+        stringBuilder.AppendLine("Name        : " + Interface.Name);
+        stringBuilder.AppendLine("Description : " + Interface.Description);
+        stringBuilder.AppendLine("ID          : " + Interface.Id);
+        stringBuilder.AppendLine("Status      : " + Interface.OperationalStatus);
+        stringBuilder.AppendLine("================================================");
+      }
+
+      return stringBuilder.ToString();
+    }
+
+  }
+}
