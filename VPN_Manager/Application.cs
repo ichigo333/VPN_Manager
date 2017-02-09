e28e08a54637590ed89ac586d7f3bcb7f2a23818
Alex <alex333sh@gmail.com>
2017-02-09 02:07:29 -0500

    splitting out application logic

---
+++ VPN_Manager/Application.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System;
+using System.Threading;
+
+namespace VPN_Manager {
+  public class Application {
+    public void Start(string path) {
+      Process.Start(path);
+    }
+
+    public void Kill(string name) {
+      var processes = Process.GetProcessesByName(name);
+
+      foreach (var process in processes) {
+        process.Kill();
+      }
+    }
+
+    public void StartVpn(Vpn vpn, string vpnPath) {
+      Process.Start(vpnPath);
+
+      while (!vpn.IsConnected()) {
+        Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
+        Thread.Sleep(10000);
+      }
+
+      Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
+      Console.WriteLine(vpn);
+    }
+  }
+}
--- VPN_Manager/Program.cs
+++ VPN_Manager/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Net;
 using System.Threading;
 
@@ -8,48 +7,26 @@ namespace VPN_Manager {
     public static void Main() {
       var vpn = new Vpn();
       var vpnPath = "C:\\Program Files\\pia_manager\\pia_manager.exe";
+      var application = new Application();
+
 
       while (true) {
-        KillApplication("pia_nw");
-        KillApplication("pia_manager.exe");
+        application.Kill("pia_nw");
+        application.Kill("pia_manager.exe");
 
         Thread.Sleep(10000);
-        StartVpn(vpn, vpnPath);
+        application.StartVpn(vpn, vpnPath);
 
         while (vpn.IsConnected()) {
           Console.Write("VPN is connected");
-          for (int i=0; i < 20; i++) {
+          for (int i = 0; i < 20; i++) {
             Console.Write(".");
             Thread.Sleep(1000);
           }
           Console.WriteLine();
         }
       }
-     
-    }
 
-    public static void StartVpn(Vpn vpn, string vpnPath) {
-      StartApplication(vpnPath);
-
-      while (!vpn.IsConnected()) {
-        Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
-        Thread.Sleep(10000);
-      }
-
-      Console.WriteLine("Is VPN Connected : " + vpn.IsConnected());
-      Console.WriteLine(vpn);
-    }
-
-    public static void StartApplication(string path) {
-      Process.Start(path);
-    }
-
-    public static void KillApplication(string name) {
-      var processes = Process.GetProcessesByName(name);
- 
-      foreach (var process in processes) {
-        process.Kill();
-      }
     }
 
     public static string GetCurrentIP() {
