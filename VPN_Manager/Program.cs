3c7a65f8da3a516b756113a9b447e9f0486942a2
Alex <alex333sh@gmail.com>
2017-02-11 20:22:43 -0500

    switching to deault VS spacing

--- VPN_Manager/Program.cs
+++ VPN_Manager/Program.cs
@@ -3,38 +3,38 @@ using System.Net;
 using System.Threading;
 
 namespace VPN_Manager {
-  class MainClass {
-    public static void Main() {
-      var vpn = new Vpn();
-      var vpnPath = "C:\\Program Files\\pia_manager\\pia_manager.exe";
-      var application = new Application();
+    class MainClass {
+        public static void Main() {
+            var vpn = new Vpn();
+            var vpnPath = "C:\\Program Files\\pia_manager\\pia_manager.exe";
+            var application = new Application();
 
 
-      while (true) {
-        application.Kill("pia_nw");
-        application.Kill("pia_manager.exe");
+            while (true) {
+                application.Kill("pia_nw");
+                application.Kill("pia_manager.exe");
 
-        Thread.Sleep(10000);
-        application.StartVpn(vpn, vpnPath);
+                Thread.Sleep(10000);
+                application.StartVpn(vpn, vpnPath);
 
-        while (vpn.IsConnected()) {
-          Console.Write("VPN is connected");
-          for (int i = 0; i < 20; i++) {
-            Console.Write(".");
-            Thread.Sleep(1000);
-          }
-          Console.WriteLine();
+                while (vpn.IsConnected()) {
+                    Console.Write("VPN is connected");
+                    for (int i = 0; i < 20; i++) {
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
         }
-      }
 
+        public static string GetCurrentIP() {
+            var client = new WebClient();
+            client.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.BypassCache);
+
+            string downloadString = client.DownloadString("https://api.ipify.org");
+            return downloadString;
+        }
     }
-
-    public static string GetCurrentIP() {
-      var client = new WebClient();
-      client.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.BypassCache);
-
-      string downloadString = client.DownloadString("https://api.ipify.org");
-      return downloadString;
-    }
-  }
 }
--- VPN_Manager/Vpn.cs
+++ VPN_Manager/Vpn.cs
@@ -2,42 +2,42 @@
 using System.Text;
 
 namespace VPN_Manager {
-  public class Vpn {
+    public class Vpn {
 
-    public bool IsConnected() {
-      if (GetVpnInterface() != null) {
-        return true;
-      }
-      return false;
-    }
-
-    NetworkInterface GetVpnInterface() {
-      if (NetworkInterface.GetIsNetworkAvailable()) {
-        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
-        foreach (NetworkInterface Interface in interfaces) {
-          if (Interface.OperationalStatus == OperationalStatus.Up && Interface.Description == "TAP-Windows Adapter V9") {
-            return Interface;
-          }
+        public bool IsConnected() {
+            if (GetVpnInterface() != null) {
+                return true;
+            }
+            return false;
         }
-      }
-      return null;
+
+        NetworkInterface GetVpnInterface() {
+            if (NetworkInterface.GetIsNetworkAvailable()) {
+                NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+                foreach (NetworkInterface Interface in interfaces) {
+                    if (Interface.OperationalStatus == OperationalStatus.Up && Interface.Description == "TAP-Windows Adapter V9") {
+                        return Interface;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public override string ToString() {
+            var Interface = GetVpnInterface();
+            var stringBuilder = new StringBuilder();
+
+            if (Interface != null) {
+                stringBuilder.AppendLine("================================================");
+                stringBuilder.AppendLine("Name        : " + Interface.Name);
+                stringBuilder.AppendLine("Description : " + Interface.Description);
+                stringBuilder.AppendLine("ID          : " + Interface.Id);
+                stringBuilder.AppendLine("Status      : " + Interface.OperationalStatus);
+                stringBuilder.AppendLine("================================================");
+            }
+
+            return stringBuilder.ToString();
+        }
+
     }
-
-    public override string ToString() {
-      var Interface = GetVpnInterface();
-      var stringBuilder = new StringBuilder();
-
-      if (Interface != null) {
-        stringBuilder.AppendLine("================================================");
-        stringBuilder.AppendLine("Name        : " + Interface.Name);
-        stringBuilder.AppendLine("Description : " + Interface.Description);
-        stringBuilder.AppendLine("ID          : " + Interface.Id);
-        stringBuilder.AppendLine("Status      : " + Interface.OperationalStatus);
-        stringBuilder.AppendLine("================================================");
-      }
-
-      return stringBuilder.ToString();
-    }
-
-  }
 }
